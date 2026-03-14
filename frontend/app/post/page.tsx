ce5b58e5fef2da2118d569cfb580b6941699c0ae
Kyle Yuen <kyuen4@sfsu.edu>
2026-03-13 23:18:09 -0700

    added post page and updated it a bit, can create post but wont be saved

--- frontend/app/post/page.tsx
+++ frontend/app/post/page.tsx
@@ -5,10 +5,39 @@ export default function PostPage(){
 
     if (!isLoggedIn){
         return (
-            <main>
-                <h1 className={styles.title}>Post
-                    <p>You must login to create a post</p>
-                </h1>
+            <main className={styles.container}>
+                <h1 className={styles.title}>Create a Travel Post</h1>
+                <p>Share a place you visited.</p>
+
+                <form className={styles.form}>
+
+                    <div className={styles.field}>
+                        <label htmlFor="title">Title</label>
+                        <input id="title" type="text" className={styles.input} />
+                    </div>
+
+                    <div className={styles.field}>
+                        <label htmlFor="city">City</label>
+                        <input id="city" type="text" className={styles.input} />
+                    </div>
+
+                    <div className={styles.field}>
+                        <label htmlFor="country">Country</label>
+                        <input id="country" type="text" className={styles.input} />
+                    </div>
+
+                    <div className={styles.field}>
+                        <label htmlFor="description">Description</label>
+                        <textarea id="description" className={styles.textarea}/>
+                    </div>
+
+                    <div className={styles.field}>
+                        <label htmlFor="imageUrl">Image URL</label>
+                        <input id="imageUrl" type="text" className={styles.input} />
+                    </div>
+                    
+                    <button type="submit" className={styles.button}> Create Post </button>
+                </form>
             </main>
         );
     }
